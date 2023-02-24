589ea83fe03c1ca2661c2779bb413ae228722763
Mosi <mostafawshe@gmail.com>
2023-02-24 14:32:06 +0330

    Destinations Comp

---
+++ components/DestinationsComp.vue
@@ -0,0 +1,114 @@
+<template>
+    <div class="container">
+        <section class="destination">
+                    <div class="destination__header">
+                        <img src="/images/svgs/destination.svg" alt="" class="destination__img" loading="lazy">
+                        <h2 class="destination__title">Explore Destinations & Activities</h2>
+                    </div>
+                    <div class="destination__wrapper">
+                        <div class="destination__item">
+                            <div class="destination__item-banner">
+                                <img src="/images/image1.jpg" alt="place" class="destination__item-img" loading="lazy">
+                                <div class="destination__item-like">
+                                    <img src="/images/svg-icons/heart.svg" alt="heart"
+                                        class="destination__item-like__icon" loading="lazy">
+                                </div>
+                                <div class="destination__item-price">
+                                    <p class="destination__item-price-text">$36/night</p>
+                                </div>
+                            </div>
+                            <div class="destination__item-title">The Stuga</div>
+                            <div class="destination__item-text">Book unique camping experiences on over 300,000
+                                campsites.</div>
+                            <a href="#" class="btn destination__item-btn">Read More</a>
+                        </div>
+
+                        <div class="destination__item">
+                            <div class="destination__item-banner">
+                                <img src="/images/image2.jpg" alt="" class="destination__item-img" loading="lazy">
+                                <div class="destination__item-like">
+                                    <img src="/images/svg-icons/heart.svg" alt="" class="destination__item-like__icon"
+                                        loading="lazy">
+                                </div>
+                                <div class="destination__item-price">
+                                    <p class="destination__item-price-text">$36/night</p>
+                                </div>
+                            </div>
+                            <div class="destination__item-title">Tiny Cabin in the mountains</div>
+                            <div class="destination__item-text">Book unique camping experiences on over 300,000
+                                campsites.</div>
+                            <a href="#" class="btn destination__item-btn">Read me</a>
+                        </div>
+
+                        <div class="destination__item">
+                            <div class="destination__item-banner">
+                                <img src="/images/image3.jpg" alt="" class="destination__item-img" loading="lazy">
+                                <div class="destination__item-like">
+                                    <img src="/images/svg-icons/heart.svg" alt="" class="destination__item-like__icon"
+                                        loading="lazy">
+                                </div>
+                                <div class="destination__item-price">
+                                    <p class="destination__item-price-text">$36/night</p>
+                                </div>
+                            </div>
+                            <div class="destination__item-title">Dragonfly Tiny Cabin</div>
+                            <div class="destination__item-text">Book unique camping experiences on over 300,000
+                                campsites.</div>
+                            <a href="#" class="btn destination__item-btn">Read me</a>
+                        </div>
+
+                        <div class="destination__item">
+                            <div class="destination__item-banner">
+                                <img src="/images/image4.jpg" alt="" class="destination__item-img" loading="lazy">
+                                <div class="destination__item-like">
+                                    <img src="/images/svg-icons/heart.svg" alt="" class="destination__item-like__icon"
+                                        loading="lazy">
+                                </div>
+                                <div class="destination__item-price">
+                                    <p class="destination__item-price-text">$36/night</p>
+                                </div>
+                            </div>
+                            <div class="destination__item-title">Tiny Cabin in the mountains</div>
+                            <div class="destination__item-text">Book unique camping experiences on over 300,000
+                                campsites.</div>
+                            <a href="#" class="btn destination__item-btn">Read me</a>
+                        </div>
+
+                        <div class="destination__item">
+                            <div class="destination__item-banner">
+                                <img src="/images/image5.jpg" alt="" class="destination__item-img" loading="lazy">
+                                <div class="destination__item-like">
+                                    <img src="/images/svg-icons/heart.svg" alt="" class="destination__item-like__icon"
+                                        loading="lazy">
+                                </div>
+                                <div class="destination__item-price">
+                                    <p class="destination__item-price-text">$36/night</p>
+                                </div>
+                            </div>
+                            <div class="destination__item-title">The Hermitage Cabin</div>
+                            <div class="destination__item-text">Book unique camping experiences on over 300,000
+                                campsites.</div>
+                            <a href="#" class="btn destination__item-btn">Read me</a>
+                        </div>
+
+                        <div class="destination__item">
+                            <div class="destination__item-banner">
+                                <img src="/images/image6.jpg" alt="" class="destination__item-img" loading="lazy">
+                                <div class="destination__item-like">
+                                    <img src="/images/svg-icons/heart.svg" alt="" class="destination__item-like__icon"
+                                        loading="lazy">
+                                </div>
+                                <div class="destination__item-price">
+                                    <p class="destination__item-price-text">$36/night</p>
+                                </div>
+                            </div>
+                            <div class="destination__item-title">Kindred Spirits Cabin</div>
+                            <div class="destination__item-text">Book unique camping experiences on over 300,000
+                                campsites.</div>
+                            <a href="#" class="btn destination__item-btn">Read me</a>
+                        </div>
+                    </div>
+                    <a href="#" class="btn destination__btn">View all</a>
+                </section>
+    </div>
+</template>

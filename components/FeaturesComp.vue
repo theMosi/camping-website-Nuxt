a67351ae86f443f0c2cf31e4af0eb6b0ad277aa9
Mosi <mostafawshe@gmail.com>
2023-02-24 14:31:52 +0330

    FeatureComp

---
+++ components/FeaturesComp.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <section class="services">
+                    <div class="service">
+                        <img src="/images/svgs/fire.svg" class="service__img" alt="Camping" loading="lazy">
+                        <h2 class="service__title">Camping & Day Use</h2>
+                        <p class="service__text">Hmm Hpsum is a fictional text by me and you gonna read this because I
+                            decide
+                            this for you.</p>
+                    </div>
+                    <div class="service">
+                        <img src="/images/svgs/tickets.svg" class="service__img" alt="Camping" loading="lazy">
+                        <h2 class="service__title">Your & Tickets</h2>
+                        <p class="service__text">Hmm Hpsum is a fictional text by me and you gonna read this because I
+                            decide
+                            this for you.</p>
+                    </div>
+                    <div class="service">
+                        <img src="/images/svgs/paper.svg" class="service__img" alt="Camping" loading="lazy">
+                        <h2 class="service__title">Permits</h2>
+                        <p class="service__text">Hmm Hpsum is a fictional text by me and you gonna read this because I
+                            decide
+                            this for you.</p>
+                    </div>
+                    <div class="service">
+                        <img src="/images/svgs/fish.svg" class="service__img" alt="Camping" loading="lazy">
+                        <h2 class="service__title">Recreation Activities</h2>
+                        <p class="service__text">Hmm Hpsum is a fictional text by me and you gonna read this because I
+                            decide
+                            this for you.</p>
+                    </div>
+                </section>
+    </div>
+</template>
